565c8c845b541fe3ba6003013c58397f292d63f8
turtlebot <turtlebot@huey.(none)>
2016-01-21 15:48:40 -0500

    Added setup file

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+line=$(ifconfig wlan0 | grep 'inet addr')
+re="inet addr:([0-9\.]*)"
+if [[ $line =~ $re ]];
+then
+	ip_addr=${BASH_REMATCH[1]};
+else
+	echo "Could not find ip address"
+	exit 1
+fi
+
+export ROS_HOSTNAME=$ip_addr
+export ROS_MASTER_URI="http://$ip_addr:11311"
+
+source .bashrc
