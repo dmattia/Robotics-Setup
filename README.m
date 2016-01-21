5f8a27b425f9d8511e7647ce68b38d67a71e96c4
turtlebot <turtlebot@huey.(none)>
2016-01-21 17:24:54 -0500

    Added clearer error message and allow to run from any directory

--- README.m
+++ README.m
@@ -1,6 +1,6 @@
 Setup file for PART A of Getting Started Section
 
 TO RUN:
-    Place in same directory as .bashrc file
+    The first time you run this file, you must run 'chmod u+x ./setup.sh' to get the correct permissions
     Run the command '. ./setup.sh'
-    This first dot is very important, otherwise running ./setup.sh; source .bashrc will work
+    This first dot is very important, otherwise running ./setup.sh; source ~/.bashrc will work
--- setup.sh
+++ setup.sh
@@ -7,10 +7,12 @@ then
 	ip_addr=${BASH_REMATCH[1]};
 else
 	echo "Could not find ip address"
+	echo "Please check network connection and try again"
 	exit 1
 fi
 
 export ROS_HOSTNAME=$ip_addr
 export ROS_MASTER_URI="http://$ip_addr:11311"
 
-source .bashrc
+source ~/.bashrc
+echo "Success! ip address is $ip_addr"
