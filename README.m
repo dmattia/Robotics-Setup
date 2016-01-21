8d4ac5608839f69a96288a5f6f0bd22b08da63f4
turtlebot <turtlebot@huey.(none)>
2016-01-21 15:50:56 -0500

    Added README

---
+++ README.m
@@ -0,0 +1,6 @@
+Setup file for PART A of Getting Started Section
+
+TO RUN:
+    Place in same directory as .bashrc file
+    Run the command '. ./setup.sh'
+    This first dot is very important, otherwise running ./setup.sh; source .bashrc will work
