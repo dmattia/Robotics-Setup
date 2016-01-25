5ae0c681a0c39698452d3a7fc2092035b32688f0
turtlebot <turtlebot@dewey.(none)>
2016-01-25 14:57:35 -0500

    Moved tmux installation into setup.sh

--- install_tmux.sh
+++
@@ -1,9 +0,0 @@
-echo "Checking if tmux installed..."
-if ! which tmux > /dev/null;
-then
-	echo "Installing tmux..."
-	sudo apt-get install tmux
-	echo "Success: tmux installed"
-else
-	echo "tmux is already installed"
-fi
--- setup.sh
+++ setup.sh
@@ -16,3 +16,16 @@ export ROS_MASTER_URI="http://$ip_addr:11311"
 
 source ~/.bashrc
 echo "Success! ip address is $ip_addr"
+
+# Install tmux if not yet
+echo "Checking if tmux installed..."
+if ! which tmux > /dev/null;
+then
+	echo "Installing tmux..."
+	sudo apt-get install tmux
+	echo "Success: tmux installed"
+else
+	echo "tmux is already installed"
+fi
+
+echo "set -g mouse-select-pane on" > ~/.tmux.conf
