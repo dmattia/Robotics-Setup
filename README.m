93ce1c256c146827e07a52b4fc8b5afd1a15b504
turtlebot <turtlebot@dewey.(none)>
2016-01-25 15:02:17 -0500

    Added new features to README

--- README.m
+++ README.m
@@ -1,6 +1,7 @@
-Setup file for PART A of Getting Started Section
+Scripts to complete the "Turtlebot Setup and Getting Started" packet
 
 TO RUN:
-    The first time you run this file, you must run 'chmod u+x ./setup.sh' to get the correct permissions
-    Run the command '. ./setup.sh'
-    This first dot is very important, otherwise running ./setup.sh; source ~/.bashrc will work
+    The first time you run this file, you must run 'chmod u+x ./setup.sh' and 'chmod u+x ./setup_tmux.sh' to get the correct permissions
+    Run the command '. ./setup.sh'. This first dot is very important!
+    If you are prompted for a password, user the password you use to login to the laptop, not your netid password.
+    Run the command '.setup_tmux.sh'
