cd4c08bbedfa7827d50613e9054a3d6fe146e207
turtlebot <turtlebot@dewey.(none)>
2016-01-25 15:13:49 -0500

    Force kill any suspended processes

--- README.m
+++ README.m
@@ -1,7 +1,11 @@
 Scripts to complete the "Turtlebot Setup and Getting Started" packet
 
 TO RUN:
-    The first time you run this file, you must run 'chmod u+x ./setup.sh' and 'chmod u+x ./setup_tmux.sh' to get the correct permissions
+    change into the root directory of this folder with 'cd Robotics-Setup'
+    The first time you run this file, you must run 'chmod u+x ./setup.sh ./setup_tmux.sh' to get the correct permissions
+    Turn your robot on
     Run the command '. ./setup.sh'. This first dot is very important!
     If you are prompted for a password, user the password you use to login to the laptop, not your netid password.
-    Run the command '.setup_tmux.sh'
+    Run the command './setup_tmux.sh'
+
+    That's it! If you are new to tmux, you can switch between panes in the terminal with your mouse. To close a terminal, use C-c to kill any process that is still running and then click C-b then x then y when it asks for confirmation. Make sure to close out of the last pane.
